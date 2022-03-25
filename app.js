2e8056922f0754c4572fa45bbf123dddcf2ed1a0
joaoorei <joao.costa3@estudante.ifms.edu.br>
2022-03-25 11:36:54 -0400

    Adicionando o Portal de Noticias

---
+++ app.js
@@ -0,0 +1,8 @@
+const app=require('./config/server');
+const rotaHome=require('./app/routes/home')(app);
+const rotaAdmin=require('./app/routes/admin')(app);
+const rotaNoticias=require('./app/routes/noticias')(app);
+
+app.listen('3000',function(){
+	console.log('Servidor rodando na porta 3000');
+});
---
+++ app/routes/admin.js
@@ -0,0 +1,9 @@
+module.exports=function(app){
+
+
+app.get('/formularioinclusaonoticia',(req,res)=>{
+	res.render('admin/form_add_noticia.ejs');
+});
+
+
+}
